4a1b4e2215a29d1b79a2fbcd76424232c786823a
chrisgrizzy <christopherg95@me.com>
2017-01-24 19:56:10 -0500

    Mario Sound Mixer

--- play.js
+++ play.js
@@ -14,7 +14,25 @@ buttonElement.addEventListener('click', function () {
 audio3.play(),console.log('Button 3  was clicked!')
 })
 
+var buttonElement = document.getElementById('button4')
+buttonElement.addEventListener('mouseenter', function () {
+audio4.play(),console.log('Button 4  was hovered over')
+})
+
+var buttonElement = document.getElementById('button5')
+buttonElement.addEventListener('mouseenter', function () {
+audio5.play(),console.log('Button 5  was hovered over')
+})
+
+var buttonElement = document.getElementById('button6')
+buttonElement.addEventListener('mouseenter', function () {
+audio6.play(),console.log('Button 6 was hovered over')
+})
+
 
 var audio1 = new Audio('coin.wav')
 var audio2 = new Audio('powerup.wav')
 var audio3 = new Audio('mario.wav')
+var audio4 = new Audio('hehoo.wav')
+var audio5 = new Audio('weehee.wav')
+var audio6 = new Audio('yehoo.wav')
