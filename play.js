7e3c926c776068391aeaece12445e9d805342eeb
chrisgrizzy <christopherg95@me.com>
2017-01-24 22:36:18 -0500

    Updated Event Listeners, Alt Tag, and Debug

--- play.js
+++ play.js
@@ -1,38 +1,49 @@
 var buttonElement = document.getElementById('button1')
-buttonElement.addEventListener('click', function () {
- audio1.play(),
-  console.log('Button 1  was clicked!')
-})
+buttonElement.addEventListener('click', myfunction);
+buttonElement.addEventListener('mouseenter', myfunction);
+function myfunction(){
+ audio1.play()
+}
 
 var buttonElement = document.getElementById('button2')
-buttonElement.addEventListener('click', function () {
-audio2.play(),console.log('Button 2  was clicked!')
-})
+buttonElement.addEventListener('click', myfunction2);
+buttonElement.addEventListener('mouseenter', myfunction2);
+function myfunction2(){
+ audio2.play()
+}
 
 var buttonElement = document.getElementById('button3')
-buttonElement.addEventListener('click', function () {
-audio3.play(),console.log('Button 3  was clicked!')
-})
+buttonElement.addEventListener('click', myfunction3);
+buttonElement.addEventListener('mouseenter', myfunction3);
+function myfunction3(){
+ audio3.play()
+}
 
 var buttonElement = document.getElementById('button4')
-buttonElement.addEventListener('mouseenter', function () {
-audio4.play(),console.log('Button 4  was hovered over')
-})
+buttonElement.addEventListener('click', myfunction4);
+buttonElement.addEventListener('mouseenter', myfunction4);
+function myfunction4(){
+ audio4.play()
+}
 
 var buttonElement = document.getElementById('button5')
-buttonElement.addEventListener('mouseenter', function () {
-audio5.play(),console.log('Button 5  was hovered over')
-})
+buttonElement.addEventListener('click', myfunction5);
+buttonElement.addEventListener('mouseenter', myfunction5);
+function myfunction5(){
+ audio5.play()
+}
 
 var buttonElement = document.getElementById('button6')
-buttonElement.addEventListener('mouseenter', function () {
-audio6.play(),console.log('Button 6 was hovered over')
-})
+buttonElement.addEventListener('click', myfunction6);
+buttonElement.addEventListener('mouseenter', myfunction6);
+function myfunction6(){
+ audio6.play()
+}
 
 
-var audio1 = new Audio('coin.wav')
-var audio2 = new Audio('powerup.wav')
-var audio3 = new Audio('mario.wav')
-var audio4 = new Audio('hehoo.wav')
-var audio5 = new Audio('weehee.wav')
-var audio6 = new Audio('yehoo.wav')
+var audio1 = new Audio('sounds/coin.wav')
+var audio2 = new Audio('sounds/powerup.wav')
+var audio3 = new Audio('sounds/mario.wav')
+var audio4 = new Audio('sounds/hehoo.wav')
+var audio5 = new Audio('sounds/weehee.wav')
+var audio6 = new Audio('sounds/yehoo.wav')
